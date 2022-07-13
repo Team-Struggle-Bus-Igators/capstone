07fb88b9fb8480d40c31e080b21541f4d1c8179d
CJOcode <cjoconnor24@gmail.com>
2022-07-13 14:23:43 -0700

    finished home trello card, added assets component, Home page still needs styling

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
+import HomePagePhoto from '../assets/HomePagePhoto2.jpg'
 
-export default class Home extends Component {
+
+class Home extends Component {
   render() {
     return (
-      <div>Home</div>
+      <>
+        <div className='home__component'>
+          <h1>Welcome to Learnify</h1>
+          <img src={HomePagePhoto} alt="HomePagePhoto"/>
+          <div className='call__to__action'>
+            <ul>
+              <NavLink to="/postindex"><button>Learnify Yourself</button></NavLink>
+              <NavLink to="/postnew"><button >Learnify Others</button></NavLink>
+            </ul>
+          </div>
+          <div className='body'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+
+
+        </div>
+      </>
     )
   }
 }
+
+export default Home

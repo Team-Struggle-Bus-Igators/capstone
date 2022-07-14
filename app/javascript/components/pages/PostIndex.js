b76f538393387d8420096095a96f6be21f8e845e
Renzlawr <RSLogan23@gmail.com>
2022-07-13 19:02:28 -0600

    added state changes for later conditional rendering, fixed warning

--- app/javascript/components/pages/PostIndex.js
+++ app/javascript/components/pages/PostIndex.js
@@ -22,19 +22,17 @@ class PostIndex extends Component {
     this.toggle = this.toggle.bind(this);
     this.state = {
       dropdownOpen: false,
-      // units: {
-        unit1: false,
-        unit2: false,
-        unit3: false,
-        unit4: false,
-        unit5: false,
-        unit6: false,
-        unit7: false,
-        unit8: false,
-        unit9: false,
-        unit10: false,
-        unit11: false,
-      // }
+      unit1: false,
+      unit2: false,
+      unit3: false,
+      unit4: false,
+      unit5: false,
+      unit6: false,
+      unit7: false,
+      unit8: false,
+      unit9: false,
+      unit10: false,
+      unit11: false,
     };
   }
 
@@ -44,14 +42,7 @@ class PostIndex extends Component {
     });
   }
 
-  handleChange = (unit) => {
-    () => this.setState({
-      [unit]: true
-    });
-  }
-
   render() {
-    console.log("unit1", this.state.unit1)
     return (
     <>
       <div className='unprotected__index__component'>
@@ -63,39 +54,37 @@ class PostIndex extends Component {
           </DropdownToggle>
           <DropdownMenu>
             <DropdownItem header>Select Unit</DropdownItem>
-            <DropdownItem onChange={this.handleChange(this.state.unit1)}>Unit 1: JS Introduction</DropdownItem>
+            <DropdownItem onClick={() => this.setState({unit1: true})}>Unit 1: JS Introduction</DropdownItem>
             <DropdownItem divider />
-            <DropdownItem>Unit 2: JS Foundations</DropdownItem>
+            <DropdownItem onClick={() => this.setState({unit2: true})}>Unit 2: JS Foundations</DropdownItem>
             <DropdownItem divider />
-            <DropdownItem>Unit 3: React</DropdownItem>
+            <DropdownItem onClick={() => this.setState({unit3: true})}>Unit 3: React</DropdownItem>
             <DropdownItem divider />
-            <DropdownItem>Unit 4: Ruby</DropdownItem>
+            <DropdownItem onClick={() => this.setState({unit4: true})}>Unit 4: Ruby</DropdownItem>
             <DropdownItem divider />
-            <DropdownItem>Unit 5:Intro to Postgres and Ruby on Rails Models</DropdownItem>
+            <DropdownItem onClick={() => this.setState({unit5: true})}>Unit 5:Intro to Postgres and Ruby on Rails Models</DropdownItem>
             <DropdownItem divider />
-            <DropdownItem>Unit 6: Ruby on Rails</DropdownItem>
+            <DropdownItem onClick={() => this.setState({unit6: true})}>Unit 6: Ruby on Rails</DropdownItem>
             <DropdownItem divider />
-            <DropdownItem>Unit 7: Professional Development Week</DropdownItem>
+            <DropdownItem onClick={() => this.setState({unit7: true})}>Unit 7: Professional Development Week</DropdownItem>
             <DropdownItem divider />
-            <DropdownItem>Unit 8: Cat Tinder! Combining React and Rails</DropdownItem>
+            <DropdownItem onClick={() => this.setState({unit8: true})}>Unit 8: Cat Tinder! Combining React and Rails</DropdownItem>
             <DropdownItem divider />
-            <DropdownItem>Unit 9: React in Rails and Authentication</DropdownItem>
+            <DropdownItem onClick={() => this.setState({unit9: true})}>Unit 9: React in Rails and Authentication</DropdownItem>
             <DropdownItem divider />
-            <DropdownItem>Unit 10: Capstone Project, MVP</DropdownItem>
+            <DropdownItem onClick={() => this.setState({unit10: true})}>Unit 10: Capstone Project, MVP</DropdownItem>
             <DropdownItem divider />
-            <DropdownItem>Unit 11: Capstone Project, Icebox</DropdownItem>
+            <DropdownItem onClick={() => this.setState({unit11: true})}>Unit 11: Capstone Project, Icebox</DropdownItem>
           </DropdownMenu>
           </ButtonDropdown>
         </div>
         <div className='index__cards'>
         <CardGroup>
           <Card     
-          body 
           color="info"
           inverse
           >
             <CardHeader 
-            body
             className="text-center"
             >
               Topic
@@ -127,12 +116,10 @@ class PostIndex extends Component {
             </CardBody>
           </Card>
           <Card     
-          body
           color="info"
           inverse
           >
             <CardHeader
-            body
             className="text-center"
             >
               Topic
@@ -164,12 +151,10 @@ class PostIndex extends Component {
             </CardBody>
           </Card>
           <Card
-          body
           color="info"
           inverse
           >
             <CardHeader
-            body
             className="text-center"
             >
               Topic

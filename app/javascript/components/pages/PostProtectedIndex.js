a5dd0b83b0e8a04b7cd2bc1506071da225e9145f
CJOcode <cjoconnor24@gmail.com>
2022-07-18 12:05:51 -0700

    added myposts cards

--- app/javascript/components/pages/PostProtectedIndex.js
+++ app/javascript/components/pages/PostProtectedIndex.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react'
 
-export default class PostProtectedIndex extends Component {
+class PostProtectedIndex extends Component {
   render() {
+    const { posts, current_user } = this.props
+    const usersPosts = posts.filter(post => post.user_id === current_user.id)
     return (
-      <div>PostProtectedIndex</div>
+      <>
+        <h1>These are your current posts</h1>
+        <div id="grid-protected-index">
+          {usersPosts && posts &&
+            posts.map((post, index) => {
+            return <Card id="index-card" key={index}>
+                <CardBody id="card-body-index">
+                <CardTitle>{post.topic}</CardTitle>
+                <CardSubtitle>{post.unit}</CardSubtitle>
+                <CardSubtitle>{post.post_type}</CardSubtitle>
+                <CardSubtitle>{post.date}</CardSubtitle>
+                <CardImg id="card-image" src={post.image} alt="Card image cap" />
+                <NavLink id="info-button" to={`/postshow/${post.id}`}>
+                    More Info
+                </NavLink>
+                </CardBody>
+              </Card>
+            })
+          }
+        </div>
+      </>
     )
   }
 }
+
+
+export default PostProtectedIndex

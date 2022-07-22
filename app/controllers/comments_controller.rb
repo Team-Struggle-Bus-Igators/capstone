a38e11e1f67fdb12bce70728991ccd22ce9e1fbf
Renzlawr <RSLogan23@gmail.com>
2022-07-22 13:04:59 -0600

    added a bit of the backend for comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,36 @@
+class CommentsController < ApplicationController
+    def index
+        comments = Comment.all.order('created_at DESC')
+        render json: comments
+    end 
+
+    def create
+        comment = Comment.create(comment_params)
+        if comment.valid? 
+            render json: comment
+        else
+            render json: comment.errors, status: 422
+        end
+    end
+
+    def update
+        comment = Comment.find(params[:id])
+        comment.update(comment_params)
+        if comment.valid? 
+            render json: comment
+        else
+            render json: comment.errors, status: 422
+        end
+    end
+    
+    def destroy
+        comment = Comment.find(params[:id])
+        comment.destroy
+        render json: {destroy: true}
+    end
+
+    private
+    def comment_params
+        params.require(:comment).permit(:remark,:post_id, :user_id)
+    end
+end
